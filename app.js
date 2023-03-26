60fdd7e03c6f125a7414d2d690ff6b0f1cc9b03d
sandeep yadav <bhojpuriyasandeep95@gmail.com>
2023-03-26 13:04:05 +0530

    first-commit

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-        const heading = React.createElement("h1",{id:"heading"},"hello using react");
+        const heading = React.createElement("div",{id:"parent"},[React.createElement("h1",{id:"heading"},"child content"),React.createElement("p",{id:"paregraph"},"this is peragraph")]);
+
+
+
+
+
         const root = ReactDOM.createRoot(document.getElementById("root"));
         root.render(heading);
